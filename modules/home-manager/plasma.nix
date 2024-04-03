0611a4462eb7c8a3c3eead4d24bf29a5ff21dfa9
HexoKnight <harvey.gream@gmail.com>
2024-04-03 14:50:42 +0100

    more plasma config

--- modules/home-manager/plasma.nix
+++ modules/home-manager/plasma.nix
@@ -17,6 +17,7 @@
     configFile = {
       # extra cursor stuff
       "kcminputrc"."Mouse"."cursorTheme".value = "Win10OS-cursors";
+
       # fonts
       "kdeglobals"."General"."fixed".value = "Monospace,10,-1,5,50,0,0,0,0,0";
       "kdeglobals"."General"."font".value = "Sans Serif,10,-1,5,50,0,0,0,0,0";
@@ -28,6 +29,29 @@
       # night light
       "kwinrc"."NightColor"."Active".value = true;
       "kwinrc"."NightColor"."Mode".value = "Constant";
+
+      # faster animations
+      "kdeglobals"."KDE"."AnimationDurationFactor".value = 0.25;
+
+      # alt-tab menu
+      "kwinrc"."TabBox"."LayoutName".value = "thumbnail_grid";
+
+      # change window opacity on scroll (titlebar or Meta + anywhere else)
+      "kwinrc"."MouseBindings"."CommandAllWheel".value = "Change Opacity";
+      "kwinrc"."MouseBindings"."CommandTitlebarWheel".value = "Change Opacity";
+
+      # don't show logout screen
+      "ksmserverrc"."General"."confirmLogout".value = false;
+
+      # window rules
+      "kwinrulesrc"."General"."count".value = 1;
+      "kwinrulesrc"."General"."rules".value = "871c18ba-a0ff-464f-8698-351caad3e3d8";
+      # gvim maximising
+      "kwinrulesrc"."871c18ba-a0ff-464f-8698-351caad3e3d8"."Description".value = "gvim maximised";
+      "kwinrulesrc"."871c18ba-a0ff-464f-8698-351caad3e3d8"."wmclass".value = "gvim";
+      "kwinrulesrc"."871c18ba-a0ff-464f-8698-351caad3e3d8"."wmclassmatch".value = 1;
+      "kwinrulesrc"."871c18ba-a0ff-464f-8698-351caad3e3d8"."strictgeometryrule".value = 2;
+      "kwinrulesrc"."871c18ba-a0ff-464f-8698-351caad3e3d8"."types".value = 1;
     };
   };
 }
