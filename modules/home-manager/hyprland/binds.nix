1cbade0a35b1a41f0f104da2a3be866cee005d2c
HexoKnight <harvey.gream@gmail.com>
2024-05-11 01:16:13 +0100

    use `SUPER` directly without `$mainMod`
    just not necessary ¯\_(ツ)_/¯

--- modules/home-manager/hyprland/binds.nix
+++ modules/home-manager/hyprland/binds.nix
@@ -7,38 +7,37 @@
         allow_workspace_cycles = true;
       };
 
-      "$mainMod" = "SUPER";
       bindr = [
-        "$mainMod, grave, exec, hyprctl keyword input:follow_mouse 2"
+        "SUPER, grave, exec, hyprctl keyword input:follow_mouse 2"
       ];
       bind = [
-        "$mainMod, grave, exec, hyprctl keyword input:follow_mouse 1"
+        "SUPER, grave, exec, hyprctl keyword input:follow_mouse 1"
 
-        "$mainMod, Q, killactive, "
-        "$mainMod, T, exec, ${config.home.sessionVariables.TERMINAL}"
-        "$mainMod CTRL ALT, XKB_KEY_Delete, exit, "
-        "$mainMod, E, exec, lf"
-        "$mainMod, V, togglefloating, "
-        "$mainMod, R, exec, rofi -show run"
-        "$mainMod SHIFT, R, exec, rofi -show drun"
-        "$mainMod, P, pseudo, "
-        "$mainMod, S, layoutmsg, togglesplit"
+        "SUPER, Q, killactive, "
+        "SUPER, T, exec, ${config.home.sessionVariables.TERMINAL}"
+        "SUPER CTRL ALT, XKB_KEY_Delete, exit, "
+        "SUPER, E, exec, lf"
+        "SUPER, V, togglefloating, "
+        "SUPER, R, exec, rofi -show run"
+        "SUPER SHIFT, R, exec, rofi -show drun"
+        "SUPER, P, pseudo, "
+        "SUPER, S, layoutmsg, togglesplit"
 
-        ''$mainMod SHIFT, S, exec, ${pkgs.grim}/bin/grim -g "$(${pkgs.slurp}/bin/slurp)"''
+        ''SUPER SHIFT, S, exec, ${pkgs.grim}/bin/grim -g "$(${pkgs.slurp}/bin/slurp)"''
 
-        "$mainMod, M, focusmonitor, +1"
-        "$mainMod SHIFT, M, movecurrentworkspacetomonitor, +1"
+        "SUPER, M, focusmonitor, +1"
+        "SUPER SHIFT, M, movecurrentworkspacetomonitor, +1"
 
         # Example special workspace (scratchpad)
-        # "$mainMod, S, togglespecialworkspace, magic"
-        # "$mainMod SHIFT, S, movetoworkspace, special:magic"
+        # "SUPER, S, togglespecialworkspace, magic"
+        # "SUPER SHIFT, S, movetoworkspace, special:magic"
 
         # Scroll through existing workspaces with mainMod + scroll
-        "$mainMod, mouse_down, workspace, e+1"
-        "$mainMod, mouse_up, workspace, e-1"
+        "SUPER, mouse_down, workspace, e+1"
+        "SUPER, mouse_up, workspace, e-1"
 
         # ALT-tab / SUPER-tab
-        "$mainMod, tab, workspace, previous"
+        "SUPER, tab, workspace, previous"
         "ALT, tab, workspace, previous"
       ]
 
@@ -62,7 +61,7 @@
       ++ (builtins.concatMap (directionList: with with lib; builtins.listToAttrs
         ((lists.zipListsWith attrsets.nameValuePair) ["arrowKey" "homeKey" "hyprDir"] directionList);
         builtins.concatMap (key: [
-          "$mainMod, ${key}, movefocus, ${hyprDir}"
+          "SUPER, ${key}, movefocus, ${hyprDir}"
         ]) [arrowKey homeKey]
       ) [
         ["left"  "h" "l"]
@@ -78,16 +77,16 @@
           key = builtins.toString (if x == 9 then 0 else x + 1);
         in [
           # Switch workspaces with mainMod + [0-9]
-          "$mainMod, ${key}, workspace, ${workspacenum}"
+          "SUPER, ${key}, workspace, ${workspacenum}"
           # Move active window to a workspace with mainMod + SHIFT + [0-9]
-          "$mainMod SHIFT, ${key}, movetoworkspace, ${workspacenum}"
+          "SUPER SHIFT, ${key}, movetoworkspace, ${workspacenum}"
         ]
       ) 10);
 
       bindm = [
         # Move/resize windows with mainMod + LMB/RMB and dragging
-        "$mainMod, mouse:272, movewindow"
-        "$mainMod, mouse:273, resizewindow"
+        "SUPER, mouse:272, movewindow"
+        "SUPER, mouse:273, resizewindow"
       ];
     };
   };
