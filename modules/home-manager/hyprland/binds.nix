81e1440cac3cc349fc50f3a275e88b98292813f6
HexoKnight <harvey.gream@gmail.com>
2024-05-13 01:32:23 +0100

    update main binds to use hyprbinds

--- modules/home-manager/hyprland/binds.nix
+++ modules/home-manager/hyprland/binds.nix
@@ -1,95 +1,97 @@
 { config, system-config, lib, pkgs, inputs, ... }:
 
 {
+  hyprbinds = with lib.hyprbinds; lib.mkMerge [{
+    "SUPER, grave" = [
+      (mkExec "hyprctl keyword input:follow_mouse 1")
+      (withFlags "r" mkExec "hyprctl keyword input:follow_mouse 2")
+    ];
+    "SUPER, Q" = mkNoArgBind "killactive";
+    "SUPER, T" = mkExec config.home.sessionVariables.TERMINAL;
+    "SUPER CTRL ALT, XKB_KEY_Delete" = mkNoArgBind "exit";
+    "SUPER, E" = mkExec "lf";
+    "SUPER, V" = mkNoArgBind "togglefloating";
+    "SUPER, R" = mkExec "rofi -show run";
+    "SUPER SHIFT, R" = mkExec "rofi -show drun";
+    "SUPER, P" = mkNoArgBind "pseudo";
+    "SUPER, S" = mkBind "layoutmsg" "togglesplit";
+
+    "SUPER SHIFT, S" = mkExec ''${pkgs.grim}/bin/grim -g "$(${pkgs.slurp}/bin/slurp)"'';
+
+    "SUPER, M" = mkBind "focusmonitor" "+1";
+    "SUPER SHIFT, M" = mkBind "movecurrentworkspacetomonitor" "+1";
+
+    # Example special workspace (scratchpad)
+    # "SUPER, S" = mkBind "togglespecialworkspace" "magic";
+    # "SUPER SHIFT, S" = mkBind "movetoworkspace" "special:magic";
+
+    # Scroll through existing workspaces with mainMod + scroll
+    "SUPER, mouse_down" = mkBind "workspace" "e+1";
+    "SUPER, mouse_up" = mkBind "workspace" "e-1";
+
+    # Move/resize windows with mainMod + LMB/RMB and dragging
+    "SUPER, mouse:272" = mkMouseBind "movewindow";
+    "SUPER, mouse:273" = mkMouseBind "resizewindow";
+
+    # ALT-tab / SUPER-tab
+    "SUPER, tab" = mkBind "workspace" "previous";
+    "ALT, tab" = mkBind "workspace" "previous";
+  }
+
+  (let
+    brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
+  in {
+    ", XF86KbdBrightnessUp"   = mkExec "${brightnessctl} -d *::kbd_backlight s +1";
+    ", XF86KbdBrightnessDown" = mkExec "${brightnessctl} -d *::kbd_backlight s 1-";
+    ", XF86MonBrightnessUp"   = mkExec "${brightnessctl} s +5%";
+    ", XF86MonBrightnessDown" = mkExec "${brightnessctl} s 5%-";
+  })
+
+  (let
+    swayosd = "${pkgs.swayosd}/bin/swayosd";
+  in {
+    ", XF86AudioRaiseVolume" = mkExec "${swayosd} --output-volume raise";
+    ", XF86AudioLowerVolume" = mkExec "${swayosd} --output-volume lower";
+    ", XF86AudioMute"        = mkExec "${swayosd} --output-volume mute-toggle";
+  })
+
+  (let
+    mergeMap = f: list: lib.attrsets.mergeAttrsList (map f list);
+  in mergeMap (directionList: with with lib; builtins.listToAttrs
+    ((zipListsWith nameValuePair) ["arrowKey" "homeKey" "hyprDir" "resize"] directionList);
+    # allows a function to take a function as an argument instead of an object
+    # hmm = func: (f: f f) (self: arg: if builtins.isFunction arg then arg2: (self self) (arg arg2) else func arg)
+    # (t: t t t t "0") (hmm (a: a + "!"))
+    mergeMap (key: {
+      "SUPER, ${key}" = mkBind "movefocus" hyprDir;
+      "SUPER SHIFT, ${key}" = mkBind "swapwindow" hyprDir;
+      "SUPER CTRL, ${key}" = mkBind "resizeactive" (resize "10");
+    }) [arrowKey homeKey]
+  ) [
+    ["left"  "h" "l" (amount: "-${amount} 0")]
+    ["down"  "j" "d" (amount: "0 ${amount}")]
+    ["up"    "k" "u" (amount: "0 -${amount}")]
+    ["right" "l" "r" (amount: "${amount} 0")]
+  ])
+
+  (lib.attrsets.mergeAttrsList (builtins.genList (
+    x:
+    let
+      workspacenum = builtins.toString (x + 1);
+      key = builtins.toString (if x == 9 then 0 else x + 1);
+    in {
+      # Switch workspaces with mainMod + [0-9]
+      "SUPER, ${key}" = mkBind "workspace" workspacenum;
+      # Move active window to a workspace with mainMod + SHIFT + [0-9]
+      "SUPER SHIFT, ${key}" = mkBind "movetoworkspace" workspacenum;
+    }
+  ) 10))
+  ];
   wayland.windowManager.hyprland = {
-    settings = with pkgs; {
+    settings = {
       binds = {
         allow_workspace_cycles = true;
       };
-
-      bindr = [
-        "SUPER, grave, exec, hyprctl keyword input:follow_mouse 2"
-      ];
-      bind = [
-        "SUPER, grave, exec, hyprctl keyword input:follow_mouse 1"
-
-        "SUPER, Q, killactive, "
-        "SUPER, T, exec, ${config.home.sessionVariables.TERMINAL}"
-        "SUPER CTRL ALT, XKB_KEY_Delete, exit, "
-        "SUPER, E, exec, lf"
-        "SUPER, V, togglefloating, "
-        "SUPER, R, exec, rofi -show run"
-        "SUPER SHIFT, R, exec, rofi -show drun"
-        "SUPER, P, pseudo, "
-        "SUPER, S, layoutmsg, togglesplit"
-
-        ''SUPER SHIFT, S, exec, ${pkgs.grim}/bin/grim -g "$(${pkgs.slurp}/bin/slurp)"''
-
-        "SUPER, M, focusmonitor, +1"
-        "SUPER SHIFT, M, movecurrentworkspacetomonitor, +1"
-
-        # Example special workspace (scratchpad)
-        # "SUPER, S, togglespecialworkspace, magic"
-        # "SUPER SHIFT, S, movetoworkspace, special:magic"
-
-        # Scroll through existing workspaces with mainMod + scroll
-        "SUPER, mouse_down, workspace, e+1"
-        "SUPER, mouse_up, workspace, e-1"
-
-        # ALT-tab / SUPER-tab
-        "SUPER, tab, workspace, previous"
-        "ALT, tab, workspace, previous"
-      ]
-
-      ++ (let
-        brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
-      in [
-        ", XF86KbdBrightnessUp,   exec, ${brightnessctl} -d *::kbd_backlight s +1"
-        ", XF86KbdBrightnessDown, exec, ${brightnessctl} -d *::kbd_backlight s 1-"
-        ", XF86MonBrightnessUp,   exec, ${brightnessctl} s +5%"
-        ", XF86MonBrightnessDown, exec, ${brightnessctl} s 5%-"
-      ])
-
-      ++ (let
-        swayosd = "${pkgs.swayosd}/bin/swayosd";
-      in [
-        ", XF86AudioRaiseVolume, exec, ${swayosd} --output-volume raise"
-        ", XF86AudioLowerVolume, exec, ${swayosd} --output-volume lower"
-        ", XF86AudioMute,        exec, ${swayosd} --output-volume mute-toggle"
-      ])
-
-      ++ (builtins.concatMap (directionList: with with lib; builtins.listToAttrs
-        ((lists.zipListsWith attrsets.nameValuePair) ["arrowKey" "homeKey" "hyprDir" "resize"] directionList);
-        builtins.concatMap (key: [
-          "SUPER, ${key}, movefocus, ${hyprDir}"
-          "SUPER SHIFT, ${key}, swapwindow, ${hyprDir}"
-          "SUPER CTRL, ${key}, resizeactive, ${resize "10"}"
-        ]) [arrowKey homeKey]
-      ) [
-        ["left"  "h" "l" (amount: "-${amount} 0")]
-        ["down"  "j" "d" (amount: "0 ${amount}")]
-        ["up"    "k" "u" (amount: "0 -${amount}")]
-        ["right" "l" "r" (amount: "${amount} 0")]
-      ])
-
-      ++ builtins.concatLists (builtins.genList (
-        x:
-        let
-          workspacenum = builtins.toString (x + 1);
-          key = builtins.toString (if x == 9 then 0 else x + 1);
-        in [
-          # Switch workspaces with mainMod + [0-9]
-          "SUPER, ${key}, workspace, ${workspacenum}"
-          # Move active window to a workspace with mainMod + SHIFT + [0-9]
-          "SUPER SHIFT, ${key}, movetoworkspace, ${workspacenum}"
-        ]
-      ) 10);
-
-      bindm = [
-        # Move/resize windows with mainMod + LMB/RMB and dragging
-        "SUPER, mouse:272, movewindow"
-        "SUPER, mouse:273, resizewindow"
-      ];
     };
   };
 }
