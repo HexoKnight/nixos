a9a58da11f0e037f942a7cb5d72fab07fee319b1
HexoKnight <harvey.gream@gmail.com>
2024-05-15 22:43:31 +0100

    add some save linking stuff
    to be able to link save files declaratively without changing the nix config every 10 seconds

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -114,6 +114,7 @@ in {
     # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
   }
+
   (attrsets.optionalAttrs (persistence) {
     home.persistence."/persist/home/${username}" = {
       allowOther = true;
@@ -133,6 +134,7 @@ in {
         "Pictures"
         "Videos"
         "Music"
+        "Saves"
         ".config/vesktop"
         ".config/google-chrome"
         # cache for logged in accounts and stuff
@@ -141,11 +143,11 @@ in {
           directory = ".local/share/Steam";
           method = "symlink";
         }
-        ".config/unity3d"
         ".config/termusic"
       ];
     };
   })
+
   (attrsets.optionalAttrs (desktop) {
     home.packages = with pkgs; [
       (google-chrome.override {
@@ -188,10 +190,118 @@ in {
       package = pkgs.callPackage "${inputs.self}/packages/Win10OS-cursors.nix" {};
     };
   })
-  (attrsets.optionalAttrs (personal-gaming) {
+
+  (attrsets.optionalAttrs (personal-gaming) (
+  let
+    declareDefault = name: default: ''${name}="''${${name}:-${default}}"'';
+    declare-LINKED_SAVES_DIR = declareDefault "LINKED_SAVES_DIR" "$HOME/Saves";
+    declare-LINKED_SAVES_LIST = declareDefault "LINKED_SAVES_LIST" "$LINKED_SAVES_DIR/list.json";
+
+    jq = pkgs.jq + "/bin/jq";
+    xargs = pkgs.findutils + "/bin/xargs";
+    sponge = pkgs.moreutils + "/bin/sponge";
+
+    linkSaveDir = lib.scripts.mkScript pkgs "linkSaveDir" ''
+      ${declare-LINKED_SAVES_DIR}
+
+      test -n "$1" || {
+        >&2 echo 'the save location must be passed as the first parameter'
+        exit 1
+      }
+      test -n "$2" || {
+        >&2 echo 'the save name must be passed as the second parameter'
+        exit 1
+      }
+      save_dir="$(realpath -s "$1")"
+      save_name="$2"
+      linked_save_dir="''${LINKED_SAVES_DIR}/''${save_name}"
+
+      function ifnotquiet() {
+        test ! -n "$LINK_SAVE_QUIET" && "$@"
+      }
+
+      current_linked_dir="$(readlink "$save_dir")" &&
+      test "$current_linked_dir" = "$linked_save_dir" && {
+        ifnotquiet echo "'$save_dir' already correctly linked to '$linked_save_dir'"
+        exit 0
+      }
+
+      # check if no file exists then make parent directories
+      # or if it is an empty directory then delete it
+      function ensureAvailable() {
+        {
+          test ! -e "$1" &&
+          mkdir -p "$(dirname "$1")"
+        } || {
+          test -n "$(find "$1" -maxdepth 0 -empty)" &&
+          rmdir "$1"
+        }
+      }
+
+      if ensureAvailable "$save_dir"; then
+        ifnotquiet echo "'$save_dir' empty..."
+        mkdir -p "$linked_save_dir"
+      elif ensureAvailable "$linked_save_dir"; then
+        ifnotquiet echo "'$save_dir' not empty but '$linked_save_dir' is so moving the former to the latter..."
+        mv -T "$save_dir" "$linked_save_dir"
+      else
+        # even if quiet
+        echo "files present in both normal dir ('$save_dir') and linked dir ('$linked_save_dir')"
+        echo "backing up the linked files and using the normal ones"
+        mv --backup=numbered -T "$save_dir" "$linked_save_dir"
+      fi
+      ln -s -T "$linked_save_dir" "$save_dir" &&
+      ifnotquiet echo "successfully linked '$save_dir' to '$linked_save_dir'"
+    '';
+    linkSaveDirs = lib.scripts.mkScript pkgs "linkSaveDirs" ''
+      ${declare-LINKED_SAVES_DIR}
+      ${declare-LINKED_SAVES_LIST}
+
+      test ! -e "$LINKED_SAVES_LIST" && {
+        test ! -n "$LINK_SAVE_QUIET" && echo "linked saves list ('$LINKED_SAVES_LIST') not found"
+        # exit 0
+      }
+
+      cat "$LINKED_SAVES_LIST" |
+      ${jq} --raw-output0 '
+        to_entries .[] |
+        (.value, .key)
+      ' |
+      ${xargs} -0 -L2 ${linkSaveDir}
+    '';
+    addLinkedSave = lib.scripts.mkScript pkgs "addLinkedSave" ''
+      ${declare-LINKED_SAVES_DIR}
+      ${declare-LINKED_SAVES_LIST}
+
+      test -n "$1" || {
+        >&2 echo 'the save location must be passed as the first parameter'
+        exit 1
+      }
+      save_dir="$(realpath -s "$1")"
+      save_name="''${2:-$(basename "$save_dir")}"
+
+      test ! -e "$LINKED_SAVES_LIST" && echo '{}' >"$LINKED_SAVES_LIST"
+
+      cat "$LINKED_SAVES_LIST" |
+      ${jq} --arg name "$save_name" --arg dir "$save_dir" '
+        . += {$name: $dir}
+      ' | ${sponge} "$LINKED_SAVES_LIST"
+
+      ${linkSaveDir} "$save_dir" "$save_name"
+    '';
+  in
+  {
     home.packages = with pkgs; [
       unstable.vesktop
       termusic
+      linkSaveDir
+      linkSaveDirs
+      addLinkedSave
     ];
-  })];
+
+    home.activation.linkSaves = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      $DRY_RUN_CMD ${linkSaveDirs}
+    '';
+  }))
+];
 }
