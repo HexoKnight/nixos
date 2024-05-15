462d158f1b40b9c4b607dde3c2f49504a34cb5e8
HexoKnight <harvey.gream@gmail.com>
2024-05-15 22:50:30 +0100

    add each flake input to the flake registry

--- modules/misc/nix-settings.nix
+++ modules/misc/nix-settings.nix
@@ -1,5 +1,9 @@
+{ inputs, ... }:
+
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.auto-optimise-store = true;
   nix.channel.enable = false; # only flakes :)
+
+  nix.registry = builtins.mapAttrs (_name: value: { flake = value; }) inputs;
 }
