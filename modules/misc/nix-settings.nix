37cb59bd03abab8d6a6e6dbc048e83d40bcf33d2
HexoKnight <harvey.gream@gmail.com>
2024-05-23 15:34:49 +0100

    nix optimise and gc weekly
    and disable auto-optimising the store

--- modules/misc/nix-settings.nix
+++ modules/misc/nix-settings.nix
@@ -2,8 +2,18 @@
 
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.settings.auto-optimise-store = true;
+  # nix.settings.auto-optimise-store = true;
   nix.channel.enable = false; # only flakes :)
+
+  nix.optimise = {
+    automatic = true;
+    dates = [ "weekly" ];
+  };
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
 
   nix.registry = builtins.mapAttrs (_name: value: { flake = value; }) inputs;
 }
