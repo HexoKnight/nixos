3a3c7481d3bb27f5a8945a64d504c7728026f8a7
HexoKnight <harvey.gream@gmail.com>
2024-06-02 13:42:04 +0100

    remove hyprland cache
    not currently using the hyprland flake

--- flake.nix
+++ flake.nix
@@ -67,10 +67,10 @@
       wsl = {};
     } [
       {
-        nix.settings = {
-          substituters = ["https://hyprland.cachix.org"];
-          trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-        };
+        # nix.settings = {
+        #   substituters = ["https://hyprland.cachix.org"];
+        #   trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+        # };
       }
     ];
   };
