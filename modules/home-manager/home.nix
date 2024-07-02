48903247c6df20e938562f224de1ae69fdb7eb36
HexoKnight <harvey.gream@gmail.com>
2024-07-02 14:31:11 +0100

    feat(bash): use a more generic $HISTIGNORE

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -189,8 +189,8 @@ in {
     programs.bash = {
       enable = true;
       historyControl = [ "ignorespace" "erasedups" ];
-      historyIgnore = [ "exit" ":q" ];
       initExtra = ''
+      historyIgnore = [ "exit" "?" "??" ];
         alias bathelp='bat --plain --language=help'
         function help() {
           "$@" --help 2>&1 | bathelp
