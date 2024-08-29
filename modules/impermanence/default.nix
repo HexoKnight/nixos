3ad2a26cbf60010b409675a601d2e82c52854212
HexoKnight <harvey.gream@gmail.com>
2024-08-29 02:04:27 +0100

    feat(impermanence): only keep old roots for 14 days
    instead of a month

--- modules/impermanence/default.nix
+++ modules/impermanence/default.nix
@@ -47,7 +47,7 @@ in
           btrfs subvolume delete "$1"
       }
 
-      for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+      for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +14); do
           delete_subvolume_recursively "$i"
       done
 
