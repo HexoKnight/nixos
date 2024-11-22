19547900b909f5005e2b4cc8b6a3d53d76b3bb4f
HexoKnight <harvey.gream@gmail.com>
2024-11-22 15:05:13 +0000

    refactor(mkNixosConfiguration): supply config_name through _module.args
    more modular

--- nixosConfigurations/mkNixosConfigurations.nix
+++ nixosConfigurations/mkNixosConfigurations.nix
@@ -6,12 +6,15 @@ extraModules:
 lib.mapAttrs (config_name: extraOptions: lib.nixosSystem (
   {
     specialArgs = {
-      inherit inputs config_name;
+      inherit inputs;
     };
     modules = [
       ./${config_name}
       ../modules/nixos
       {
+        _module.args = {
+          inherit config_name;
+        };
       }
     ] ++ extraModules;
   }
