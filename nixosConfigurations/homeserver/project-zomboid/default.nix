df0ca6c96f0019b3f0b52e820db38013ea2465f8
HexoKnight <harvey.gream@gmail.com>
2024-12-18 12:45:22 +0000

    feat(homeserver): add zomboid systemd service

--- nixosConfigurations/homeserver/project-zomboid/default.nix
+++ nixosConfigurations/homeserver/project-zomboid/default.nix
@@ -1,10 +1,16 @@
-{ pkgs, config, ... }:
+{ lib, pkgs, config, ... }:
 
 let
   # https://github.com/nix-community/steam-fetcher
   steam-fetcher-flake = builtins.getFlake "github:nix-community/steam-fetcher/12f66eafb7862d91b3e30c14035f96a21941bd9c";
 
   zomboid-server = pkgs.callPackage ./dedicated-server.nix {};
+
+  dataDir = "/var/lib/Zomboid";
+  socketPath = "/run/zomboid/control";
+
+  user = "pzuser";
+  group = "pzuser";
 in
 {
   config = {
@@ -15,5 +21,66 @@ in
     environment.systemPackages = [
       zomboid-server
     ];
+
+    users.users.${config.setups.config.username}.extraGroups = [ group ];
+
+    users.users.${user} = {
+      isSystemUser = true;
+      group = group;
+    };
+    users.groups.${group} = {};
+
+    # based on: https://pzwiki.net/wiki/Dedicated_server#Systemd
+    systemd = {
+      tmpfiles.settings.zomboid = {
+        "${dataDir}".d = {
+          mode = "770";
+          inherit user group;
+        };
+      };
+      services.zomboid = {
+        description = "Project Zomboid Server";
+        wants = [ "network-online.target" ];
+        after = [ "network-online.target" ];
+
+        serviceConfig = {
+          PrivateTmp = true;
+          Type = "exec";
+          User = user;
+          Group = group;
+          KillSignal = "SIGCONT";
+
+          WorkingDirectory = dataDir;
+          ReadOnlyPaths = [ "/" ];
+          ReadWritePaths = [
+            dataDir
+            "/tmp"
+          ];
+
+          ExecStart = "${lib.getExe zomboid-server} -cachedir=${dataDir}";
+          StandardInput = "file:" + socketPath;
+          # shouldn't be necessary but just in case
+          Sockets = [ "zomboid.socket" ];
+        };
+
+        preStop = ''
+          echo quit >${socketPath}
+        '';
+      };
+      sockets.zomboid = {
+        bindsTo    = [ "zomboid.service" ];
+        before     = [ "zomboid.service" ];
+        requiredBy = [ "zomboid.service" ];
+
+        socketConfig = {
+          ListenFIFO = socketPath;
+          RemoveOnStop = true;
+          FileDescriptorName = "control";
+          SocketMode = 0660;
+          SocketUser = user;
+          SocketGroup = group;
+        };
+      };
+    };
   };
 }
