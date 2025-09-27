25ba9c2398d227dd79ce87f49ba11f08d1215823
HexoKnight <harvey.gream@gmail.com>
2025-09-27 22:50:47 +0100

    feat(weak-machine): remove visual-paradigm and idea-ultimate

--- nixosConfigurations/weak-machine/default.nix
+++ nixosConfigurations/weak-machine/default.nix
@@ -25,30 +25,6 @@ in
   home-manager.users.${username} =
     { pkgs, ... }:
     {
-      nixpkgs.allowUnfreePkgs = [
-        "visual-paradigm"
-        "idea-ultimate"
-      ];
-
-      home.packages = [
-        pkgs.local.visual-paradigm
-        pkgs.jetbrains.idea-ultimate
-      ];
-      persist-home = {
-        directories = [
-          ".config/VisualParadigm"
-
-          # jetbrains.idea-ultimate
-          ".cache/JetBrains"
-          ".config/JetBrains"
-          ".local/share/JetBrains"
-          # I hate that this is generated
-          # afaict it isn't actually required
-          # but just in case :/
-          ".java"
-        ];
-      };
-
       setups.jupyter.enable = true;
     };
 
