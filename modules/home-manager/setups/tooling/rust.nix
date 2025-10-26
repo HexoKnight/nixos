3dcb4e9db1005d8bd8053cb13f63c9bd66c8f6f3
HexoKnight <harvey.gream@gmail.com>
2025-10-26 12:26:17 +0000

    feat(tooling/rust): allow cargoDir to be configured and add ${cargoDir}/bin to PATH

--- modules/home-manager/setups/tooling/rust.nix
+++ modules/home-manager/setups/tooling/rust.nix
@@ -2,24 +2,35 @@
 
 let
   cfg = config.setups.tooling.rust;
-
-  cargoDir = ".local/share/cargo";
 in
 {
   options.setups.tooling.rust = {
     enable = lib.mkEnableOption "rust dev stuff";
+
+    cargoDir = lib.mkOption {
+      description = "the location (relative to $HOME) of cargo's home ($CARGO_HOME)";
+      type = lib.types.pathWith {
+        absolute = false;
+        inStore = false;
+      };
+      default = ".local/share/cargo";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     persist-home = {
       directories = [
-        cargoDir
+        cfg.cargoDir
       ];
     };
 
     home.sessionVariables = {
-      CARGO_HOME = "$HOME/${cargoDir}";
+      CARGO_HOME = "$HOME/${cfg.cargoDir}";
     };
+
+    home.sessionPath = [
+      "$HOME/${cfg.cargoDir}/bin"
+    ];
 
     home.packages = with pkgs.unstable; [
       rustc
