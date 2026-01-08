78da55fb05b71c81583bf5fc6ca7d942f064456d
HexoKnight <harvey.gream@gmail.com>
2026-01-08 04:20:23 +0000

    feat(tooling/rust): patch rustup
    to allow changing rustup versions without upgrading/reinstalling all
    toolchains

--- modules/home-manager/setups/tooling/rust.nix
+++ modules/home-manager/setups/tooling/rust.nix
@@ -43,7 +43,37 @@ in
     ];
 
     home.packages = [
-      pkgs.rustup
+      # patch rustup to dynamically find the ld-wrapper (falling back to hard-coding for an attempt at back compat)
+      # prevents breakage when the rustup version is changed and the old ld-wrapper store path is removed
+      (pkgs.rustup.overrideAttrs (finalAttrs: prevAttrs: {
+        postPatch = prevAttrs.postPatch or "" + ''
+          substituteInPlace src/toolchain.rs \
+            --replace-fail \
+              'pub fn set_env(&self, cmd: &mut Command) {' \
+              'pub fn set_env(&self, cmd: &mut Command) {
+                   cmd.env("RUSTUP_EXE", &crate::utils::current_exe().unwrap());
+              '
+        '' +
+        # patching after a patch:
+        # https://github.com/NixOS/nixpkgs/blob/d351d0653aeb7877273920cd3e823994e7579b0b/pkgs/development/tools/rust/rustup/0001-dynamically-patchelf-binaries.patch
+        # 4 levels of quoting D: (nix, bash, rust, bash)
+        ''
+          substituteInPlace src/dist/component/package.rs \
+            --replace-fail \
+              '\"{}\" $@' \
+              '
+                if [ -n \"${"$"}{{RUSTUP_EXE-}}\" ]; then
+                  \"${"$"}{{RUSTUP_EXE%/*/*}}/nix-support/ld-wrapper.sh\" \"$@\"
+                else
+                  \"{}\" \"$@\"
+                fi
+              '
+        '';
+
+        # test binary is compiled single threaded for some reason
+        # and the tests themselves take ages to run
+        doCheck = false;
+      }))
 
       # a cc is necessary for building rust programs
       pkgs.gcc
